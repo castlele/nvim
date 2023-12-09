2c0c2494b95ffe301f529414d9b9b3214a3d582d
Nikita Semenov <nikita.semenov@myoffice.team>
2023-12-09 08:54:16 +0300

    update plugin manager to lazy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,29 @@
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+
+require('telescope').load_extension('fzf')
+
 local t = ":Telescope "
 local l = "<leader>"
 local k = require("utils").keymap
 
 k("n", l .. "O", t .. "find_files<CR>")
 k("n", l .. "C", t .. "colorscheme<CR>")
+vim.keymap.set(
+   "n",
+   l .. "F",
+   function()
+      require("telescope.builtin").find_files({
+         cwd = require("lazy.core.config").options.root
+      })
+   end
+)
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.keymap(mode, lhs, rhs, desc)
-    local opts = { noremap = true, silent = true, desc = desc }
-    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+   local opts = { noremap = true, silent = true, desc = desc }
+   vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 end
 
 return M
