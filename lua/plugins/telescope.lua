e1608307e2d18bf6fbf21a069fe30ffae7ccdec2
castlele <castlelecs@gmail.com>
2024-08-10 07:54:15 +0300

    [fix]: show hidden files in telescope on file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,6 +19,6 @@ local t = ":Telescope "
 local l = "<leader>"
 local utils = require("utils")
 
-utils.keymap('n', l .. "O", '<cmd>lua require("telescope.builtin").find_files({hidden = false})<CR>')
+utils.keymap('n', l .. "O", '<cmd>lua require("telescope.builtin").find_files({hidden = true})<CR>')
 utils.keymap_func('n', '<leader>F', require("telescope").extensions.live_grep_args.live_grep_args)
 utils.keymap("n", l .. "C", t .. "colorscheme<CR>")
