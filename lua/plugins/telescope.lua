dbe1320ed943682bfffaefdf8c82d36db3bf339d
castlele <castlelecs@gmail.com>
2024-12-09 06:35:56 +0300

    [feat]: update telescope plugin usage

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,38 @@
+local actions = require("telescope.actions")
+local layout = require("telescope.actions.layout")
+
 require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  }
+   defaults = {
+      layout_config = {
+         horizontal = {
+            prompt_position = "bottom",
+            width = { padding = 0 },
+            height = { padding = 0 },
+            preview_width = 0.7,
+         },
+      },
+      path_display = {
+         "filename_first",
+      },
+      mappings = {
+         i = {
+            ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-y>"] = layout.toggle_preview,
+         },
+      },
+   },
+   extensions = {
+      fzf = {
+         fuzzy = true,                    -- false will only do exact matching
+         override_generic_sorter = true,  -- override the generic sorter
+         override_file_sorter = true,     -- override the file sorter
+         case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                          -- the default case_mode is "smart_case"
+      },
+   }
 }
 
--- Install fzf first
 require('telescope').load_extension('fzf')
--- Install ripgrep first
 require("telescope").load_extension("live_grep_args")
 
 local function find_files()
@@ -33,6 +53,6 @@ local t = ":Telescope "
 local l = "<leader>"
 local utils = require("utils")
 
-utils.keymap_func('n', l .. "O", find_files)
-utils.keymap_func('n', '<leader>F', require("telescope").extensions.live_grep_args.live_grep_args)
+utils.keymap_func("n", l .. "O", find_files)
+utils.keymap_func("n", l .. "F", require("telescope").extensions.live_grep_args.live_grep_args)
 utils.keymap("n", l .. "C", t .. "colorscheme<CR>")
