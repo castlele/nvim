2e7c2d2ea0025e1e209c7d87bc2ec0fca184dcad
castlele <castlelecs@gmail.com>
2024-12-24 10:19:50 +0300

    [feat]: add to build plugin it's own edit command + refactoring

--- lua/castlelecs/build.lua
+++ lua/castlelecs/build.lua
@@ -3,16 +3,31 @@ local FM = require("cluautils.file_manager")
 local M = {}
 local buildFile = "build.lua"
 local generateCommand = "generate"
-local commands = {
+
+local function editBuildFile()
+   vim.cmd("vsplit")
+   vim.cmd("edit " .. buildFile)
+end
+
+local defaultCommands = {
+   edit = editBuildFile,
    generate = function ()
+      if FM.is_file_exists(buildFile) then
+         vim.notify(
+            "Your build.lua file is already generated. You can edit it with `:Build edit` command",
+            vim.log.levels.WARN
+         )
+         return
+      end
+
       FM.create_file(buildFile)
       FM.write_to_file(buildFile, "w", function ()
          return [[
 conf = {
 }]]
       end)
-      vim.cmd("vsplit")
-      vim.cmd("edit " .. buildFile)
+
+      editBuildFile()
    end,
 }
 
@@ -29,41 +44,51 @@ local function reloadConfigurationFile()
       pcall(fun)
    end
 
+---@diagnostic disable-next-line
    return conf ~= nil
 end
 
----@return table?
+---@return table
 local function getAvailableConfigurations()
    if not reloadConfigurationFile() then
-      return nil
+      return defaultCommands
    end
 
    local configurations = {}
 
+---@diagnostic disable-next-line
    for name, _ in pairs(conf) do
+      table.insert(configurations, name)
+   end
+
+   for name, _ in pairs(defaultCommands) do
       table.insert(configurations, name)
    end
 
    return configurations
 end
 
+---@param configurations table
+---@param command string
+---@return (fun(): string)?
+local function getCodeAsFunction(configurations, command)
+   if not configurations[command] then
+      return nil
+   end
+
+   return function () return configurations[command] end
+end
+
 
 ---@param command string
 ---@return (fun(): string)?
 function M.build(command)
-   if command == generateCommand then
-      return commands[command]
-   end
-
    if not reloadConfigurationFile() then
       return nil
    end
 
-   for name, code in pairs(conf) do
-      commands[name] = function () return code end
-   end
-
-   local fun = commands[command]
+   ---@diagnostic disable-next-line
+   local fun = getCodeAsFunction(conf, command) or defaultCommands[command]
 
    if not fun then
       vim.notify(
@@ -77,16 +102,7 @@ function M.build(command)
 end
 
 function M.completion()
-
-   if FM.is_file_exists(buildFile) then
-      local configurations = getAvailableConfigurations()
-
-      if configurations then
-         return configurations
-      end
-   end
-
-   return { generateCommand }
+   return getAvailableConfigurations()
 end
 
 
