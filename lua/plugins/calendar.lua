ff7e95a513c06e6828ff8378902557462e4920de
Nikita Semenov <nikita.semenov@myoffice.team>
2025-07-16 16:16:30 +0300

    feat(calendar_plugin): make calendar cache in the notes directory to be sync across devices

--- lua/plugins/calendar.lua
+++ lua/plugins/calendar.lua
@@ -1,3 +1,10 @@
 return {
    "itchyny/calendar.vim",
+   config = function()
+      vim.g.calendar_cache_directory = "~/notes/calendar/"
+      vim.g.calendar_cyclic_view = 1
+      vim.g.calendar_first_day = "monday"
+      vim.g.calendar_date_endian = "little"
+      vim.g.calendar_date_separator = "."
+   end,
 }
