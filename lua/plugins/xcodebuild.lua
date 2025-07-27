fffc567cfa2de4d69d9b06b25e37c4f851dcbf83
castlele <castlelecs@gmail.com>
2025-07-27 22:46:38 +0300

    feat(telescope): make telescope a dependency for xcodebuild plugin

--- lua/plugins/xcodebuild.lua
+++ lua/plugins/xcodebuild.lua
@@ -1,5 +1,8 @@
 return {
    "wojciech-kulik/xcodebuild.nvim",
+   dependencies = {
+      "nvim-telescope/telescope.nvim",
+   },
    enabled = vim.uv.os_uname().sysname == "Darwin",
    opts = true,
 }
