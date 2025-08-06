fe29c10b8fdf81beec007431722e75452f64e503
castlele <castlelecs@gmail.com>
2025-08-06 08:16:39 +0300

    fix(build.nvim): fix possible plugin configuration error on linux

--- lua/plugins/build.lua
+++ lua/plugins/build.lua
@@ -1,4 +1,8 @@
 return {
    "castlele/build.nvim",
    opts = true,
+   -- TODO: Update build.nvim with mini.picker or just deprecate the plugin itself
+   dependencies = {
+      "nvim-telescope/telescope.nvim",
+   },
 }
