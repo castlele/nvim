2b8f9d2f4a4bb93ac4fbaf00ec7470959afb59ec
castlele <castlelecs@gmail.com>
2025-09-01 08:00:11 +0500

    feat(weekOpts): update week opts

--- lua/weekOpts.lua
+++ lua/weekOpts.lua
@@ -3,6 +3,7 @@ local dailyTasks = {
    "meditate",
    "read book",
    "reflex on thoughts",
+   "play with Javie",
 }
 
 ---@type WeekConfiguration
@@ -10,9 +11,10 @@ local weekOpts = {
    week = {
       MON = {
          "work - 3h",
-         "fill stats",
          "prepare next week file",
          "take care of Javie",
+         "review goals",
+         "review week",
          daily = dailyTasks,
       },
       TUE = {
@@ -53,21 +55,23 @@ local weekOpts = {
          "comeback - 30m",
          "clean the floors",
          "clean the office",
-         "review goals",
-         "review week",
+         "fill stats",
          daily = dailyTasks,
       },
    },
    sectionFooter = string.format(
       "%s\n%s\n%s",
-      "goal: %SMART goal%",
+      "goal:",
+      "- %SMART goal%\n",
       "total: ",
       "result: "
    ),
    header = string.format(
       "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
       "@document.meta",
-      "goal: %SMART goal%",
+      "goal: [",
+      "    %SMART goal%",
+      "]",
       "stats: [",
       "    resting     : 0",
       "    work        : 0",
