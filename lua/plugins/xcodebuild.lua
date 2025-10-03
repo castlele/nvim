f49861dea51737dee05edabb19b97c2270585bfc
castlele <castlelecs@gmail.com>
2025-10-03 05:31:55 +0300

    fix(xcodebuild): add dependency for supporting new version of xcodebuild

--- lua/plugins/xcodebuild.lua
+++ lua/plugins/xcodebuild.lua
@@ -2,6 +2,7 @@ return {
    "wojciech-kulik/xcodebuild.nvim",
    dependencies = {
       "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
    },
    enabled = vim.uv.os_uname().sysname == "Darwin",
    opts = true,
