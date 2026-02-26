11ba40d3002e03d5e5f311cf6b25296a65f3d379
castlele <castlelecs@gmail.com>
2026-02-26 11:49:31 +0300

    feat(overseer/android): add task to install android application

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -5,6 +5,22 @@ return {
 
       module.setup {}
 
+      local uv = vim.loop
+      local cwd = vim.fn.getcwd()
+      local isGradleProject = uv.fs_stat(cwd .. "/settings.gradle")
+         or uv.fs_stat(cwd .. "/settings.gradle.kts")
+
+      if isGradleProject then
+         module.register_template {
+            name = "android run",
+            builder = function()
+               return {
+                  cmd = { "./gradlew", "installDebug" },
+               }
+            end,
+         }
+      end
+
       require("utils").setKeymaps {
          n = {
             ["<leader>R"] = module.run_task,
