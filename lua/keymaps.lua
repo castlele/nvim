a421d41934776e16539787665245a09803e89cb1
castlele <castlelecs@gmail.com>
2026-03-23 19:44:53 +0300

    feat(keymaps): remove default copying to + register

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,7 +4,9 @@ local customKeymaps = {
       ["th"] = vim.cmd.tabprev,
       ["tl"] = vim.cmd.tabnext,
       ["tn"] = vim.cmd.tabnew,
-      ["_"] = function() vim.cmd("Ex") end,
+      ["_"] = function()
+         vim.cmd("Ex")
+      end,
       ["<leader>yfp"] = function()
          local path = vim.fn.expand("%:p")
          vim.fn.setreg("+", path)
@@ -16,5 +18,3 @@ local customKeymaps = {
 local utils = require("utils")
 
 utils.setKeymaps(customKeymaps)
-utils.keymap("n", "y", '"+y')
-utils.keymap("v", "y", '"+y')
