df0aeaaa9586db11a750f83cc62e9f6f347f036c
castlele <castlelecs@gmail.com>
2026-05-09 20:34:18 +0300

    fix(tree-sitter): add dependency for treesitter for treesj

--- plugin/treesj.lua
+++ plugin/treesj.lua
@@ -1,3 +1,7 @@
-vim.pack.add { "https://github.com/Wansmer/treesj.git" }
+vim.pack.add {
+   "https://github.com/Wansmer/treesj.git",
+   "https://github.com/nvim-treesitter/nvim-treesitter.git",
+}
 
+require("nvim-treesitter").setup()
 require("treesj").setup()
